67b5015b5ebebc46eb5aa5ef869fdf2ae9d5e067
Abhishek Yadav <yabhishekk480@gmail.com>
2026-03-12 23:15:16 +0530

    Changed schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -20,6 +20,7 @@ export default defineSchema({
     name: v.string(),
     type: fileTypes,
     orgId: v.string(),
+    userId: v.id("users"),
     fileId: v.id("_storage"),
     shouldDelete: v.optional(v.boolean()),
   }).index("by_orgId", ["orgId"]).index("by_shouldDelete", ["shouldDelete"]),
@@ -30,6 +31,8 @@ export default defineSchema({
   }).index('by_userId_orgId_fileId', ["userId", "orgId", "fileId"]),
   users: defineTable({
     tokenIdentifier: v.string(),
+    name: v.optional(v.string()),
+    image: v.optional(v.string()),
     orgIds: v.array(v.object({
       orgId: v.string(),
       role: roles,
